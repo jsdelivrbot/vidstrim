b0b205a8919ec2fc413649f850c57018c7403c19
ggjack <gg.jack@hotmail.com>
2018-02-18 21:24:29 -0500

    initial setup

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var http = require('http');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-var server = http.createServer(function(req, res) {
-res.writeHead(200);
-res.end('Hi everybody!');
-});
-server.listen(8080);
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
